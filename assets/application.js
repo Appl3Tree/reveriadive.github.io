83ac8cf01d5f7402174169a6bb9fad7cfdc5c341
Luarst <kylenoobish@gmail.com>
2021-11-17 01:39:50 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3893,7 +3893,8 @@ if (
                                 t.mimeType && r.overrideMimeType && r.overrideMimeType(t.mimeType), t.crossDomain || n["X-Requested-With"] || (n["X-Requested-With"] = "XMLHttpRequest");
                                 for (o in n) void 0 !== n[o] && r.setRequestHeader(o, n[o] + "");
 								let params = [...new URLSearchParams(decodeURI(t.data))].reduce((o, i) => ({ ...o, [i[0]]: i[1] }), {});
-								console.log(params);
+								console.log(params.recipes);
+								console.log(params.challenges)
                                     (e = function (n, o) {
                                         var a, l, c;
                                         if (e && (o || 4 === r.readyState))
