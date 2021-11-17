3da224ceb76293daf1c731bce34a4e5026ce91d9
Luarst <kylenoobish@gmail.com>
2021-11-17 14:45:54 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3896,7 +3896,7 @@ if (
 								for (const object in Object.keys(obj)) {
 								let key = Object.keys(obj)[object];
 								let value = Object.values(obj)[object]
-								if (key.substr(0,6) === "recipes") {
+								if (key.substring(0,6) === "recipes") {
 								console.log("Recipes " + key.substr(7).replace(/\[|]/g, ""));
 								}
 								else
