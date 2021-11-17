ac1de414d4a339a054dd0f6bc6d2905231d3ca91
Luarst <kylenoobish@gmail.com>
2021-11-17 12:09:01 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3894,9 +3894,9 @@ if (
                                 for (o in n) void 0 !== n[o] && r.setRequestHeader(o, n[o] + "");
 								let remainder = []
 								for (const [key, value] of Object.entries(new URLSearchParams(t.data))) {
-								remainder[key] = value; 	
+								remainder[key] = value;
+								console.log(key, " ", value)
 								}
-								console.log(remainder);
                                     (e = function (n, o) {
                                         var a, l, c;
                                         if (e && (o || 4 === r.readyState))
