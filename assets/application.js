b5316bbb026ea48c3b8f2e36abce421f373c08d8
Luarst <kylenoobish@gmail.com>
2021-11-16 23:24:23 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3892,8 +3892,7 @@ if (
                                 if ((r.open(t.type, t.url, t.async, t.username, t.password), t.xhrFields)) for (o in t.xhrFields) r[o] = t.xhrFields[o];
                                 t.mimeType && r.overrideMimeType && r.overrideMimeType(t.mimeType), t.crossDomain || n["X-Requested-With"] || (n["X-Requested-With"] = "XMLHttpRequest");
                                 for (o in n) void 0 !== n[o] && r.setRequestHeader(o, n[o] + "");
-                                let search = t.data.search.substring(1);
-								console.log(('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}'))
+								console.log(('{"' + decodeURI(t.data).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}'))
                                     (e = function (n, o) {
                                         var a, l, c;
                                         if (e && (o || 4 === r.readyState))
