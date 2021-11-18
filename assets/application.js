79df0c595a594e26aff29646fcf61769abf51053
Luarst <kylenoobish@gmail.com>
2021-11-17 23:38:26 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3899,7 +3899,7 @@ if (
 								console.log(key + " " + value);
 								if (key.includes("recipes")) {
 								console.log("Recipes " + key.substr(7).replace(/\[|]/g, ""));
-								document.getElementById("itemList").innerHTML = '<div class="panel-body">' + key + ':' + value + '</div>';
+								document.getElementById("itemList").appendChild('<div class="panel-body">' + key + ':' + value + '</div>');
 								}
 								else {
 								console.log("Challenges " + key.substr(10).replace(/\[|]/g, ""));
