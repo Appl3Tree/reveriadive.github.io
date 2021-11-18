b48b7d539c65c1bbcfd10f480a92cc7040fae499
Luarst <kylenoobish@gmail.com>
2021-11-17 21:13:05 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3898,7 +3898,6 @@ if (
 								let value = Object.values(obj)[object]
 								console.log(key + " " + value);
 								if (key.includes("recipes")) {
-								console.log(t.text);
 								console.log("Recipes " + key.substr(7).replace(/\[|]/g, ""));
 								}
 								else {
