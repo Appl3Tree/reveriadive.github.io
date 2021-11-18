ecfbe92a52ccdc172c6f1ceb1b3a572380983acc
Luarst <kylenoobish@gmail.com>
2021-11-18 16:44:42 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3900,10 +3900,8 @@ if (
 								console.log(key + " " + value);
 								if (key.includes("recipes")) {
 								console.log("Recipes " + key.substr(7).replace(/\[|]/g, ""));
-								if (parseInt(value) >= 1) {
-								let testNode = document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, ""));
-								document.getElementById("itemList").innerHTML = `${document.getElementById("itemList").innerHTML}<div class="panel-body"><a href="${testNode.innerHTML.match(/href="([^"]*)/)[1]}">${testNode.parentNode.parentNode.children[0].innerText} : ${value}<\a></div>`;
-								}
+								if (parseInt(value) >= 1)
+								document.getElementById("itemList").innerHTML = `${document.getElementById("itemList").innerHTML}<div class="panel-body"><a href="${.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText} : ${value}</a></div>`;
 								}
 								else {
 								console.log("Challenges " + key.substr(10).replace(/\[|]/g, ""));
