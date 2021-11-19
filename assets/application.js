367d63893baf3aa857fa434f8ec100248158bb08
Luarst <kylenoobish@gmail.com>
2021-11-19 06:45:56 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3900,8 +3900,8 @@ if (
 								for (const object in Object.keys(obj)) {
 								let key = Object.keys(obj)[object];
 								let value = Object.values(obj)[object]
-								let recipeinfo = document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText;
 								if (key.includes("recipes")) {
+								let recipeinfo = document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText;
 								console.log("Recipe " + key.substr(7).replace(/\[|]/g, "") + " | " + recipes[recipeInfo][0]);
 								console.log(recipes[recipeInfo][1]);
 								console.log(`${recipeInfo}`);
