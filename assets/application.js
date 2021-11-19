d10eccacabba54a9b3d06ecac9a3bd3130d4a61f
Luarst <kylenoobish@gmail.com>
2021-11-19 09:05:32 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3902,7 +3902,7 @@ if (
 								let value = Object.values(obj)[object];
 								if (key.includes("recipes")) {
 								console.log("Recipe " + key.substr(7).replace(/\[|]/g, "") + " | ");
-								console.log(`${Object.keys(recipes)[key.substr(7).replace(/\[|]/g, "")]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
+								console.log(`${Object.values(recipes)[key.substr(7).replace(/\[|]/g, "")][1]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
 								if (parseInt(value) >= 1)
 								document.getElementById("itemList").innerHTML += `<div class="panel-body"><a href="${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText} x${value}</a></div>`;
 								}
