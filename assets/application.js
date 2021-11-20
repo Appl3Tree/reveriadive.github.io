0d95fcd4fb1939fb3ced8cf941a1d26163508180
Luarst <kylenoobish@gmail.com>
2021-11-19 22:52:42 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3905,12 +3905,13 @@ if (
 								if (item >= 90 && item <= 589) {
 								console.log("1 Recipe " + item + " | " + (Object.values(recipes)[item - 91][0]));
 								console.log(`1 ${Object.values(recipes)[item - 91][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
+								console.log(`2 ${Object.values(recipes)[item][1]}`);
 								}
 								else
 								{
-								console.log("2 Recipe " + item + " | " + (Object.values(recipes)[item][0]));
-								console.log(`2 ${Object.values(recipes)[item][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
-								console.log(`3 ${Object.values(recipes)[item][1]}`);
+								console.log("3 Recipe " + item + " | " + (Object.values(recipes)[item][0]));
+								console.log(`3 ${Object.values(recipes)[item][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
+								console.log(`4 ${Object.values(recipes)[item][1]}`);
 								}
 								document.getElementById("itemList").innerHTML += `<div class="panel-body"><a href="${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText} x${value}</a></div>`;
 									}
