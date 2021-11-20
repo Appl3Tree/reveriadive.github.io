27d9107e127450aa870b0ee94236687c033ac968
Luarst <kylenoobish@gmail.com>
2021-11-20 00:38:06 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3909,8 +3909,8 @@ if (
 								}
 								else {
 								console.log("3 Recipe " + item + " | " + (Object.values(recipes)[item][0]));
-								console.log(`3 ${Object.values(recipes)[item][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
-								console.log(`4 ${Object.values(recipes)[item][1]}`);
+								console.log(`3 ${Object.values(recipes)[item - 1][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
+								console.log(`4 ${Object.values(recipes)[item - 1][1]}`);
 								}
 								document.getElementById("itemList").innerHTML += `<div class="panel-body"><a href="${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText} x${value}</a></div>`;
 									}
