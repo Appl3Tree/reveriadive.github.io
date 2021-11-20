0e6b4d7b84e65d065f1c7c1bfe5f90cb01e08623
Luarst <kylenoobish@gmail.com>
2021-11-19 20:13:11 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3902,7 +3902,7 @@ if (
 								if (key.includes("recipes")) {
 									if (parseInt(value) >= 1) {
 								console.log("Recipe " + key.substr(7).replace(/\[|]/g, "") + " | " + key.substr(7).replace(/\[|]/g, "").parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]);
-								console.log(`${Object.values(recipes)[key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
+								console.log(`${Object.values(recipes)[key.substr(7).replace(/\[|]/g, "").parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
 								document.getElementById("itemList").innerHTML += `<div class="panel-body"><a href="${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText} x${value}</a></div>`;
 									}
 								}
