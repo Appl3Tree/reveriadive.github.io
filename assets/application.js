e5e6924c69c042d9ad30a4144966593a781291e1
Luarst <kylenoobish@gmail.com>
2021-11-19 20:35:33 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3905,7 +3905,7 @@ if (
 								if (item < 0)
 								item += 91;
 								console.log("Recipe " + item + " | ");
-								console.log(`${Object.values(recipes)[item - 91][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
+								console.log(`${Object.values(recipes)[item][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
 								document.getElementById("itemList").innerHTML += `<div class="panel-body"><a href="${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText} x${value}</a></div>`;
 									}
 								}
