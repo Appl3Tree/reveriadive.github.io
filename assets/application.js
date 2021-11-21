fe3807a206a520326dabce76f255db57b2f80b72
Luarst <kylenoobish@gmail.com>
2021-11-21 10:13:44 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3911,7 +3911,7 @@ if (
 								console.log("Recipe " + item);
 								console.log(`${Object.values(recipes)[item][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
 								console.log(`${Object.values(recipes)[item][1]}`);	
-								console.log(`${Object.values(recipes)[item][1].match(/\w+\s+/g)}`)
+								console.log(`${Object.values(recipes)[item][1].match(/\w+\s+/g).trim()}`)
 								document.getElementById("itemList").innerHTML += `<div class="panel-body"><a href="${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText} x${value}</a></div>`;
 									}
 								}
