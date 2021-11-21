57e53fca1025d884c04034c1e0e6a31f4bef33b0
Luarst <kylenoobish@gmail.com>
2021-11-21 16:08:41 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3918,7 +3918,7 @@ if (
 								console.log(item_list);
 								console.log(document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText);
 								for (const itemrecipe in item_list) {
-								document.getElementById("itemList").innerHTML += `<ol><li style="list-style-type:disc">${item_list[itemrecipe]} ×${item_amount[itemrecipe]}</li></ol>`
+								document.getElementById("itemList").innerHTML += `<ol><li style="list-style-type:disc">${item_list[itemrecipe]} ×${item_amount[itemrecipe] * value}</li></ol>`
 										}
 									}
 								}
