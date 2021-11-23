24ab2eff59333d73db2782befbe51d3d64d77fb9
Luarst <kylenoobish@gmail.com>
2021-11-22 20:04:02 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3913,14 +3913,17 @@ if (
 								// console.log(`${Object.values(recipes)[item][1]}`);
 								let item_list = Object.values(recipes)[item][1].substr(4).replace(/ \d [^\w\w+]/ig, ',').split(', ');
 								let item_amount = Object.values(recipes)[item][1].replace(/ \W w*/).match(/\d+/ig);
-								document.getElementById("itemList").innerHTML += `<h2 style="text-align:center">${Object.values(recipes)[item][0]}</h2>`
-								document.getElementById("itemList").innerHTML += `<div class="panel-body"><a href="${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}</a></div>`;
 								console.log(item_amount);
 								console.log(item_list);
 								console.log(document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText);
+								if (!document.getElementById("itemList").innerHTML.contains(Object.values(recipes)[item][0])) {
+								document.getElementById("itemList").innerHTML += `<h2 style="text-align:center">${Object.values(recipes)[item][0]}</h2>`
+								document.getElementById("itemList").innerHTML += `<div class="panel-body"><a href="${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}</a></div>`;
 								for (const itemrecipe in item_list) {
 								document.getElementById("itemList").lastChild.innerHTML += `<ol><li style="list-style-type:disc">${item_list[itemrecipe]} ×${item_amount[itemrecipe] * value}</li></ol>`
-										}
+								}
+								else
+								console.log("Duplicates!")
 									}
 								}
 								else {
