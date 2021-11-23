f7800490a1eefc757f56be611ce8f3b0697f0929
Luarst <kylenoobish@gmail.com>
2021-11-22 19:10:39 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3908,16 +3908,16 @@ if (
 								item -= 1;
 								else
 								item += 498;
-								console.log("Recipe " + item);
+								//console.log("Recipe " + item);
 								console.log(`${Object.values(recipes)[item][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
 								// console.log(`${Object.values(recipes)[item][1]}`);
 								let item_list = Object.values(recipes)[item][1].substr(4).replace(/ \d [^\w\w+]/ig, ',').split(', ');
 								let item_amount = Object.values(recipes)[item][1].replace(/ \W w*/).match(/\d+/ig);
 								document.getElementById("itemList").innerHTML += `<h2 style="text-align:center">${Object.values(recipes)[item][0]}</h2>`
 								document.getElementById("itemList").innerHTML += `<div class="panel-body" id="itemID"><a href="${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}</a></div>`;
-								console.log(item_amount);
-								console.log(item_list);
-								console.log(document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText);
+								//console.log(item_amount);
+								//console.log(item_list);
+								//console.log(document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText);
 								for (const itemrecipe in item_list) {
 								document.getElementByID.innerHTML += `<ol><li style="list-style-type:disc">${item_list[itemrecipe]} ×${item_amount[itemrecipe] * value}</li></ol>`
 								}
@@ -3929,6 +3929,7 @@ if (
 								st.add(div.textContent.trim());
 										}
 									}
+								console.log(st);
 								}
 								else {
 								console.log("Challenges " + key.substr(10).replace(/\[|]/g, ""));
