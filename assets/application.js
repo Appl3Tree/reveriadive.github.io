fb2f6da70bce302a06dad94aa458101909c1cef2
Luarst <kylenoobish@gmail.com>
2021-11-22 18:41:15 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3915,15 +3915,16 @@ if (
 								let item_amount = Object.values(recipes)[item][1].replace(/ \W w*/).match(/\d+/ig);
 								document.getElementById("itemList").innerHTML += `<h2 style="text-align:center">${Object.values(recipes)[item][0]}</h2>`
 								document.getElementById("itemList").innerHTML += `<div class="panel-body"><a href="${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerHTML.match(/href="([^"]*)/)[1]}">${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}</a></div>`;
-								console.log(item_amount);
-								console.log(item_list);
-								console.log(document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText);
+								//console.log(item_amount);
+								//console.log(item_list);
+								//console.log(document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText);
 								for (const itemrecipe in item_list) {
-								if (!key.contains(`${Object.values(recipes)[item][0]}`)) {
-								document.getElementById("itemList").innerHTML += `<ol><li style="list-style-type:disc">${item_list[itemrecipe]} ×${item_amount[itemrecipe] * value}</li></ol>`;
+								if (key.contains(`${Object.values(recipes)[item][0]}`)) {
+								document.getElementById("itemList").innerHTML.lastElementChild += `<ol><li style="list-style-type:disc">${item_list[itemrecipe]} ×${item_amount[itemrecipe] * value}</li></ol>`;
+								console.log(key);
 								}
 								else {
-								document.getElementById("itemList").innerHTML.lastElementChild += `<ol><li style="list-style-type:disc">${item_list[itemrecipe]} ×${item_amount[itemrecipe] * value}</li></ol>`
+								document.getElementById("itemList").innerHTML += `<ol><li style="list-style-type:disc">${item_list[itemrecipe]} ×${item_amount[itemrecipe] * value}</li></ol>`;
 											}
 										}
 									}
