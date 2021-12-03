1e2137d6fdc52c30f816fe2c8e4c7c277f52f972
Luarst <kylenoobish@gmail.com>
2021-12-03 02:56:18 -0600

    Update application.js

--- assets/application.js
+++ assets/application.js
@@ -3902,7 +3902,7 @@ if (
 								if (key.includes("recipes")) {
 								if (parseInt(value)) {
 								let item = Number(key.substr(7).replace(/\[|]/g, ""));
-								console.log("Recipe " + item);
+								//console.log(`${Object.values(recipes)[item][1]}`);
 								if (item >= 91 && item <= 588)
 								item -= 91;
 								else if (item >= 589)
@@ -3910,7 +3910,6 @@ if (
 								else
 								item += 498;
 								console.log(`${Object.values(recipes)[item][0]} : ${document.getElementById("recipes_" + key.substr(7).replace(/\[|]/g, "")).parentNode.parentNode.children[0].innerText}`);
-								// console.log(`${Object.values(recipes)[item][1]}`);
 								let item_list = Object.values(recipes)[item][1].substr(4).replace(/ \d [^\w\w+]/ig, ',').split(', ');
 								let item_amount = Object.values(recipes)[item][1].replace(/ \W w*/).match(/\d+/ig);
 								console.log(item_amount);
